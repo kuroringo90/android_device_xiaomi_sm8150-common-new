1da13d380b3d14b38e06979c20890e110b0c76b3
kuroringo901 <ak4700@gmx.com>
2023-10-30 00:59:58 +0100

    sdm8150-common: Build libprotobuf-cpp-lite-3.9.1 shim
    * Log
    10-07 08:02:07.069  2707  2707 W android.hardware.sensors@1.0-service: dlerror(): dlopen failed: library "libprotobuf-cpp-lite-3.9.1.so" not found: needed by /vendor/lib64/libssc.so in namespace (default)
    ...And many more
    
    add prebuilts of full
    Change-Id: I22c69b5a7da9c3e952d4db0e5a56548e0a1d47db

--- msmnile.mk
+++ msmnile.mk
@@ -361,10 +361,14 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/google/interfaces \
     hardware/google/pixel
 
-# Protobuf - Workaround for prebuilt Qualcomm HAL
+# Protobuf
 PRODUCT_PACKAGES += \
     libprotobuf-cpp-full-3.9.1-vendorcompat \
-    libprotobuf-cpp-lite-3.9.1-vendorcompat
+    libprotobuf-cpp-lite-3.9.1
+
+PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v29/arm/arch-arm-armv7-a-neon/shared/vndk-core/libprotobuf-cpp-full.so:$(TARGET_COPY_OUT_VENDOR)/lib/libprotobuf-cpp-full.so \
+    prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-full.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-full.so
 
 # Public libraries
 PRODUCT_COPY_FILES += \
