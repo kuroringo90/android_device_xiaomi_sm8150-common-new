134ee89e527051c3cbac1f605e630e1fedfdeb0f
kuroringo901 <ak4700@gmx.com>
2023-10-30 20:56:40 +0100

    sm8150-common: Debloat gapps for a14

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := Updater DocumentsUIGoogle TagGoogle LocationHistoryPrebuilt YouTube arcore PixelLiveWallpaperPrebuilt Maps
+LOCAL_OVERRIDES_PACKAGES := Updater AdaptiveVPNPrebuilt AICorePrebuilt GooglePrintRecommendationService TagGoogle TagGoogle CalculatorGooglePrebuilt DevicePolicyPrebuilt LocationHistoryPrebuilt MarkupGoogle PrebuiltGoogleTelemetryTvp SoundAmplifierPrebuilt WallpaperEmojiPrebuilt WallpaperEffect arcore talkback AmbientStreaming AndroidAutoStubPrebuilt HelpRtcPrebuilt RecorderPrebuilt SafetyHubPrebuilt ScribePrebuilt TurboAdapter TurboPrebuilt WellbeingPrebuilt EmergencyInfoGoogleNoUi 
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
